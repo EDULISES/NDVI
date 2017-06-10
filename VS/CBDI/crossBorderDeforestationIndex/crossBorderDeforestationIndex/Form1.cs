b59266f6a6d0da21cfb842ff40c8653bc89c37d9
EDULISES <edeelectronic@gmail.com>
2017-06-09 19:20:10 -0500

    NDVI, Added code to implement percentile function with linear interpolation method

--- VS/CBDI/crossBorderDeforestationIndex/crossBorderDeforestationIndex/Form1.cs
+++ VS/CBDI/crossBorderDeforestationIndex/crossBorderDeforestationIndex/Form1.cs
@@ -9,7 +9,6 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using Emgu.CV;
-using Emgu.CV.Aruco;
 using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
 using Emgu.CV.Util;
@@ -20,9 +19,20 @@ namespace crossBorderDeforestationIndex
     {
         string strDocumentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
         string defaultFolder = "";
+        // 
+        Image<Bgr, Byte> bgrImage;
+        int widthSrc;
+        int heightSrc;
+        double minPercentile = 5;
+        double maxPercentile = 95;
+        Image<Gray, Byte>[] channels;
+        Image<Gray, Byte> bChannel;
+        Image<Gray, Byte> gChannel;
+        Image<Gray, Byte> rChannel;
+        Image<Gray, double> ndvi;
 
         public Form1()
-        {            
+        {
             InitializeComponent();
             defaultFolder = strDocumentsPath + "\\Deforestation";
             imgPnl.AutoScroll = true;
@@ -42,7 +52,7 @@ namespace crossBorderDeforestationIndex
         /// This function will open a JPEG image and it will show in a pictureBox
         /// </summary>
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
-        {           
+        {
             OpenFileDialog findSrc = new OpenFileDialog();
             // Enable the JPG filter
             findSrc.Filter = "Select image|*.jpg";
@@ -50,17 +60,26 @@ namespace crossBorderDeforestationIndex
             findSrc.Title = "Open Image";
             findSrc.InitialDirectory = defaultFolder;
             if (findSrc.ShowDialog() == DialogResult.OK)
-            {          
+            {
                 // Open image with OpenCV
-                Image<Bgr, Byte> rgbImage = new Image<Bgr, byte>(findSrc.FileName);
+                bgrImage = new Image<Bgr, Byte>(findSrc.FileName);
+                widthSrc = bgrImage.Width;
+                heightSrc = bgrImage.Height;
                 // Show image in a picture Box
-                picBoxRGB.Image = rgbImage.ToBitmap();
+                picBoxRGB.Image = bgrImage.ToBitmap();
+                // Separate channels from BRG Image
+                channels = bgrImage.Split();
+                bChannel = channels[0];
+                gChannel = channels[1];
+                rChannel = channels[2];
+                // Set the size to the NDVI image
+                ndvi = new Image<Gray, double>(widthSrc, heightSrc);
             }
         }
 
         private void saveToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
@@ -71,6 +90,96 @@ namespace crossBorderDeforestationIndex
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void blueToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Show a channel into the picture box
+            picBoxRGB.Image = bChannel.ToBitmap();
+        }
+
+        private void greenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Show a channel into the picture box
+            picBoxRGB.Image = gChannel.ToBitmap();
+        }
+
+        private void redToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Show a channel into the picture box
+            picBoxRGB.Image = rChannel.ToBitmap();
+        }
+
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Show the Brg image into the picture box
+            picBoxRGB.Image = bgrImage.ToBitmap();
+        }
+
+        private void nDVIToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Red Channel + Infrared Channel
+            Image<Gray, double> bottom = (rChannel.Convert<Gray, double>() + bChannel.Convert<Gray, double>());
+            // Avoid divide by zero
+            for (int i = 0; i < heightSrc; i++)
+            {
+                for (int j = 0; j < widthSrc; j++)
+                {
+                    double pxBottom = bottom.Data[i, j, 0];
+                    if (pxBottom == 0)
+                    {
+                        bottom.Data[i, j, 0] = 0.01d;
+                    }
+                    
+                }
+            }
+            // Red Channel - Infrared Channel
+            Image<Gray, double> redDiffBlueCh = (rChannel.Convert<Gray, double>() - bChannel.Convert<Gray, double>());
+            // NDVI = (Red - Blue)/(Red + Blue) => redDiffBlueCh / bottom
+            for (int i = 0; i < heightSrc; i++)
+            {
+                for (int j = 0; j < widthSrc; j++)
+                {
+                    double pxRDiffBCh = redDiffBlueCh.Data[i, j, 0];
+                    double pxBottom = bottom.Data[i, j, 0];
+                    //System.Diagnostics.Debug.WriteLine("h:" + i + " w:" + j + "out:" + pxRDiffBCh/pxBottom);
+                    ndvi.Data[i, j, 0] = pxRDiffBCh/pxBottom;
+                }
+            }
+
+            // Convert an Image to a flat array
+            Int64 lenNdviArray = (widthSrc * heightSrc);
+            Int64 idx = 0;
+            double [] flatNdvi = new double[lenNdviArray];
+            for (int i = 0; i < heightSrc; i++)
+            {
+                for (int j = 0; j < widthSrc; j++)
+                {
+                    //System.Diagnostics.Debug.WriteLine("h:" + i + " w:" + j + "out:" + pxRDiffBCh/pxBottom);
+                    flatNdvi[idx] = ndvi.Data[i, j, 0];
+                    idx++;
+                }
+            }
+
+            // Calculate Percentile
+            Percentile centile;
+            centile = new Percentile();
+            double inMin = centile.percentileInc(flatNdvi, minPercentile);
+            double inMax = centile.percentileInc(flatNdvi, maxPercentile);
+
+            System.Diagnostics.Debug.WriteLine("min:" + inMin);
+            System.Diagnostics.Debug.WriteLine("max:" + inMax);
+            double outMin = 0;
+            double outMax = 255;
+
+            ndvi -= inMin;
+            ndvi *= ((outMin - outMax )/ (inMin - inMax));
+            ndvi += inMin;
+
+            Image<Gray, Byte> outImg = new Image<Gray, Byte>(widthSrc, heightSrc);
+            outImg = ndvi.Convert<Gray, Byte>();
+
+            picBoxRGB.Image = outImg.ToBitmap();
         }
     }
 }
---
+++ VS/CBDI/crossBorderDeforestationIndex/crossBorderDeforestationIndex/Percentile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crossBorderDeforestationIndex
+{
+    class Percentile
+    {
+        #region Public methods ...
+        /// <sumary>
+        /// Calculate the value of percentile associated with the input array.
+        /// The function use the Linear Interpolation Between Closest Ranks method.
+        /// </sumary>
+        /// <returns>
+        /// Percentile value
+        /// </returns>
+        public double percentileInc(double[] _data, double _percentile)
+        {
+            // Sort the array
+            double[] sortData = _data;
+            Array.Sort(sortData);
+
+            // First calculate the rank of the percentile
+            double noValArray = sortData.Length;
+
+            // x = ((percentile/100 )* (noValArray -1)) + 1
+            double rank = ((_percentile * (noValArray - 1d)) / 100d) + 1d;
+
+            // Get the integer part from rank
+            Int64 rankInt = (Int64)Math.Floor(rank);
+
+            // Get the decimal part from rank
+            double rankDec = rank - rankInt;
+
+            // Percentile (rank) = v(rankInt) + rankDecVal * (v(rankInt + 1) - v(rankInt))
+            // Percentile works with index 1
+            int diffIdx = 1;
+            double valPercentile = sortData[rankInt + diffIdx] + rankDec + rankDec * (sortData[(rankInt + 1) + diffIdx] - sortData[rankInt + diffIdx]);
+            return valPercentile;
+        }
+        #endregion
+    }
+}
